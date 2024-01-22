96277544fbfac4bfb9ebb740df8e5be6b74e4785
SeanGueco <156739777+SeanGueco@users.noreply.github.com>
2024-01-21 20:54:19 -0500

    Update motor_test.ino

--- motor_test/motor_test.ino
+++ motor_test/motor_test.ino
@@ -2,27 +2,31 @@
 #include <Servo.h>
 
 Servo servo1;
-int del = 50;
-float angle = 0.0;
+
+float del = 0.0;
+float angle = 91.0;
 float ans = 0.0;
 float time = 0.0;
 float diff = 0.0;
+
 void setup() {
 
   Serial.begin(9600);
   servo1.attach(3);
   servo1.write(angle);
+
 }
 
 void loop() {
+
   ans = angle;
-  angle = 90.0*sin(1.0/100.0*time) + 90.0;
+  angle = 90.0*sin(1.0/2000.0*time) + 91.0;
   servo1.write((int)angle);
 
-  diff = angle - ans;
-  del = abs(diff*(10.0/6.0));
+  diff = abs(angle - ans);
+  del = diff*(10.0/6.0);
   
-  time += (float)del;
+  time += del + 1;
   delay(del);
   
 }
