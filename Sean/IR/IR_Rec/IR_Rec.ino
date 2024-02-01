5627186723778a2e5bc487376777f1883ba505f2
SeanGueco <156739777+SeanGueco@users.noreply.github.com>
2024-01-31 20:05:03 -0500

    IR
    base code for IR

---
+++ Sean/IR/IR_Rec/IR_Rec.ino
@@ -0,0 +1,39 @@
+#include <IRremote.hpp>
+
+#define IR_RECEIVE_PIN 2
+
+
+void setup()
+{
+  // There's no need to set up the IR_RECEIVE_PIN with pinMode
+  // the library takes care of that...
+  // pinMode(IR_RECEIVE_PIN, INPUT);
+
+  Serial.begin(9600);
+
+  while(!Serial)
+  {  }
+
+  // Enable the IR receiver
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+
+  Serial.print("IR Receiver ready!");
+}
+
+void loop()
+{
+  
+
+  // decode returns 1 if something was received
+  // otherwise it returns 0
+  // The code and protocol type get stored in results
+  if (IrReceiver.decode()) {
+
+	    //Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); // Print "old" raw data
+      // USE NEW 3.x FUNCTIONS
+      IrReceiver.printIRResultShort(&Serial); // Print complete received data in one line
+      IrReceiver.printIRSendUsage(&Serial);   // Print the statement required to send this data
+      
+      IrReceiver.resume();
+  }
+}
