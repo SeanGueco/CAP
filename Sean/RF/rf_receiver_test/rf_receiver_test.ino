9806e9ac77f3e2812bcc39ef54540038e2566f9e
SeanGueco <156739777+SeanGueco@users.noreply.github.com>
2024-02-08 16:06:38 -0500

    Reorg + Add V0.1 of final code

--- Sean/RF/rf_receiver_test/rf_receiver_test.ino
+++ Sean/RF/rf_receiver_test/rf_receiver_test.ino
@@ -1,11 +1,11 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actualy used but needed to compile
 
-RH_ASK driver;
+RH_ASK driver(2000, 3, 4, 5);
 
 void setup()
 {
-    Serial.begin(9600);	// Debugging only
+    Serial.begin(115200);	// Debugging only
     driver.init();
     if (!driver.init())
          Serial.println("init failed");
@@ -13,12 +13,12 @@ void setup()
 
 void loop()
 {
-    uint8_t buf[12];
+    uint8_t buf[20];
     uint8_t buflen = sizeof(buf);
     if (driver.recv(buf, &buflen)) // Non-blocking
     {
       // Message with a good checksum received, dump it.
       Serial.print("Message: ");
       Serial.println((char*)buf);         
-    }
+    } 
 }
--- Sean/RF/rf_transmitter_test/rf_transmitter_test.ino
+++ Sean/RF/rf_transmitter_test/rf_transmitter_test.ino
@@ -1,14 +1,16 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
 
-RH_ASK driver;
+RH_ASK driver(2000, 4, 3, 5);
 
 void setup()
 {
-    Serial.begin(9600);	  // Debugging only
+  /*
+    Serial.begin(115200);	  // Debugging only
     driver.init();
     if (!driver.init())
          Serial.println("init failed");
+  */
 }
 
 void loop()
@@ -16,6 +18,7 @@ void loop()
     const char *msg = "Hello World!";
     driver.send((uint8_t *)msg, strlen(msg));
     driver.waitPacketSent();
-    delay(1000);
+    //Serial.println("Sending...");
+    delay(500);
 
 }
