2131fd05b6e1c034492a93113fb68e80cc424625
SeanGueco <156739777+SeanGueco@users.noreply.github.com>
2024-03-10 21:23:12 -0400

    naming

--- Sean/RF/rf_receiver_test/rf_receiver_test.ino
+++ Sean/RF/rf_receiver_test/rf_receiver_test.ino
@@ -1,28 +1,31 @@
 #include <RH_ASK.h>
-#include <SPI.h> // Not actualy used but needed to compile
-
-RH_ASK driver(2000, 2, 4, 5);
-
-float result = 0.0000;
-
+//#ifdef RH_HAVE_HARDWARE_SPI
+#include <SPI.h> // Not actually used but needed to compile
+//#endif
+ 
+RH_ASK driver(2000, 2, 4, 6);
+// RH_ASK driver(2000, 4, 5, 0); // ESP8266 or ESP32: do not use pin 11 or 2
+// RH_ASK driver(2000, 3, 4, 0); // ATTiny, RX on D3 (pin 2 on attiny85) TX on D4 (pin 3 on attiny85), 
+// RH_ASK driver(2000, PD14, PD13, 0); STM32F4 Discovery: see tx and rx on Orange and Red LEDS
+ 
 void setup()
 {
-    Serial.begin(115200);	// Debugging only
     driver.init();
-    if (!driver.init()) {
-         Serial.println("init failed");
-    } else {
-      Serial.println("driver begin");
-    }
-}
+    Serial.begin(115200);   // Debugging only
 
+}
+ 
 void loop()
 {
-    //uint8_t buf[20];
-    //uint8_t buflen = sizeof(buf);
-    if (driver.recv((uint8_t *)&result, 4)) // Non-blocking
+    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+    uint8_t buflen = sizeof(buf);
+ 
+    if (driver.recv(buf, &buflen)) // Non-blocking
     {
-      Serial.println(result, 4);         
-    } 
-    
+        
+ 
+        // Message with a good checksum received, dump it.
+        //driver.printBuffer("Got:", buf, buflen);
+        Serial.println((char *)buf);
+    }
 }
